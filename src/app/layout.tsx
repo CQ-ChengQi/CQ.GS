82a2b23444f7a05660d4c8cf0519cd4b492532f7
ChengQi <cq-chengqi@outlook.com>
2023-11-27 17:56:57 +0800

    调试

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="h-full bg-900 dark">
+      <body className={inter.className + " h-full"}>
         <Providers>{children}</Providers>
       </body>
     </html>
